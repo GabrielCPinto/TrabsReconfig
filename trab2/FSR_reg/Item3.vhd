8915d90ba16e881baf226c94af3622376b0d2dde
Paulo H Luiz P <54179987+pauloh-luizp@users.noreply.github.com>
2022-05-07 16:43:30 -0300

    Update Item3.vhd
    Agora tá certo!! eu sei pq eu vi no RTL Viewer

--- trab2/FSR_reg/Item3.vhd
+++ trab2/FSR_reg/Item3.vhd
@@ -17,19 +17,17 @@ END ENTITY;
 
 ARCHITECTURE fsr_reg OF Item3 IS
 BEGIN
-PROCESS(nrst, clk_in)
+PROCESS(nrst, clk_in, abus_in, dbus_in, wr_en, rd_en)
 	BEGIN
 		IF nrst = '0' THEN
 		   fsr_out <= (OTHERS => '0');
-		ELSIF RISING_EDGE(clk_in) THEN
-			IF abus_in(6 DOWNTO 0) = "0000100" AND wr_en = '1' THEN
-		       fsr_out <= dbus_in;
-		    IF abus_in(6 DOWNTO 0) = "0000100" AND rd_en = '1' THEN
-			   dbus_out <= dbus_in;
-			ELSE
-			   dbus_out <= "ZZZZZZZZ";
-			END IF;
-			END IF;
+		ELSIF RISING_EDGE(clk_in) AND abus_in(6 DOWNTO 0) = "0000100" AND wr_en = '1' THEN
+			fsr_out <= dbus_in;
+		END IF;		
+		IF abus_in(6 DOWNTO 0) = "0000100" AND rd_en = '1' THEN
+			dbus_out <= dbus_in;
+		ELSE
+			dbus_out <= "ZZZZZZZZ";
 		END IF;
 	END PROCESS;
 END fsr_reg;

0dd58accfc62ec1f53153a5da93d6579cc9c99c5
Paulo H Luiz P <54179987+pauloh-luizp@users.noreply.github.com>
2022-05-09 17:17:28 -0300

    Update Item5.vhd

--- trab2/Stack/Item5.vhd
+++ trab2/Stack/Item5.vhd
@@ -1,7 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY Item5 IS
+ENTITY Pilha IS
 	PORT (
 		nrst, clk_in : IN STD_LOGIC;
 		stack_in : IN STD_LOGIC_VECTOR(12 DOWNTO 0);
@@ -11,33 +11,42 @@ ENTITY Item5 IS
 END ENTITY;
 
 
-ARCHITECTURE stack OF Item5 IS
+ARCHITECTURE stack OF Pilha IS
+	SIGNAL stack_reg0 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg1 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg2 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg3 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg4 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg5 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg6 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	SIGNAL stack_reg7 : STD_LOGIC_VECTOR(12 DOWNTO 0);
+	CONSTANT zero : STD_LOGIC_VECTOR(12 DOWNTO 0) := "0000000000000";
 BEGIN
 PROCESS(nrst, clk_in, stack_push, stack_pop)
 	BEGIN
 		IF nrst = '0' THEN
 			stack_out <= (OTHERS => '0');
 		ELSIF RISING_EDGE(clk_in) AND (stack_pop = '1' OR stack_push = '1') THEN
-			IF stack_pop = '1' THEN
-				stack_out(0) <= stack_in(1);
-				stack_out(1) <= stack_in(2);
-				stack_out(2) <= stack_in(3);
-				stack_out(3) <= stack_in(4);
-				stack_out(4) <= stack_in(5);
-				stack_out(5) <= stack_in(6);
-				stack_out(6) <= stack_in(7);
-				stack_out(12 DOWNTO 7) <= "000000";
-			END IF;
-			IF stack_push = '1' THEN
-				stack_out(0) <= stack_in(0);
-				stack_out(1) <= stack_in(0);
-				stack_out(2) <= stack_in(1);
-				stack_out(3) <= stack_in(2);
-				stack_out(4) <= stack_in(3);
-				stack_out(5) <= stack_in(4);
-				stack_out(6) <= stack_in(5);
-				stack_out(7) <= stack_in(6);
-				stack_out(12 DOWNTO 7) <= "000000";
+			IF stack_pop = '1' AND stack_push = '1'  THEN
+				stack_out <= stack_reg0;
+				stack_reg0 <= stack_reg1;
+				stack_reg1 <= stack_reg2;
+				stack_reg2 <= stack_reg3;
+				stack_reg3 <= stack_reg4;
+				stack_reg4 <= stack_reg5;
+				stack_reg5 <= stack_reg6;
+				stack_reg6 <= stack_reg7;
+				stack_reg7 <= zero;
+
+			ELSIF stack_push = '1'  THEN
+				stack_reg7 <= stack_reg6;
+				stack_reg6 <= stack_reg5;
+				stack_reg5 <= stack_reg4;
+				stack_reg4 <= stack_reg3;
+				stack_reg3 <= stack_reg2;
+				stack_reg2 <= stack_reg1;
+				stack_reg1 <= stack_reg0;
+				stack_reg0 <= stack_in;
 			END IF;
 		END IF;
 	END PROCESS;

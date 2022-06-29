02c6fecdf4f65c82e210cfa035f5995e89945a78
GabrielCPinto <54994705+GabrielCPinto@users.noreply.github.com>
2022-06-29 12:29:32 -0300

    Add files via upload

--- trab3/Port_io/Port_io.vhd
+++ trab3/Port_io/Port_io.vhd
@@ -19,46 +19,50 @@ ENTITY port_io IS
 	);
 END ENTITY;
 
-ARCHITECTURE port_io_arch OF port_io IS
-	SIGNAL port_reg: STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL tris_reg: STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL latch: STD_LOGIC_VECTOR(7 DOWNTO 0);
+ARCHITECTURE arch OF port_io IS
+	SIGNAL port_reg : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL tris_reg : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL latch : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL i : INTEGER;--iterador do for
 BEGIN
-	PROCESS(clk_in, nrst, wr_en, abus_in, rd_en,tris_reg,port_reg,latch,port_io)
+PROCESS(nrst, clk_in, abus_in, dbus_in, wr_en, rd_en, latch, tris_reg, port_reg)
 	BEGIN
-		IF nrst = '0' THEN
+		--reset assincrono. port_reg = 0, tris_reg = 1
+		IF nrst = '0' THEN 
 			port_reg <= (OTHERS => '0');
 			tris_reg <= (OTHERS => '1');
 		ELSIF RISING_EDGE(clk_in) THEN
-			IF wr_en = '1' AND (abus_in = port_addr OR abus_in = alt_port_addr) THEN
-				port_reg <= dbus_in;
-			END IF;
-			IF wr_en = '1' AND (abus_in = tris_addr OR abus_in = alt_tris_addr) THEN
-				tris_reg <= dbus_in;
-			END IF;
+			--Operacao de escrita nos registradores tris_reg ou port_reg
+			IF wr_en = '1' AND (abus_in = port_addr OR abus_in = alt_port_addr) THEN 
+				port_reg <= dbus_in; --escrita em port_reg
+			ELSIF wr_en = '1' AND (abus_in = tris_addr OR abus_in = alt_tris_addr) THEN
+				tris_reg <= dbus_in; --escrita em tris_reg
+			END IF;			
 		END IF;
-		CASE tris_reg(0) IS WHEN '0' => port_io(0) <= port_reg(0); WHEN others => port_io(0) <= 'Z'; END CASE;
-		CASE tris_reg(1) IS WHEN '0' => port_io(1) <= port_reg(1); WHEN others => port_io(1) <= 'Z'; END CASE;
-		CASE tris_reg(2) IS WHEN '0' => port_io(2) <= port_reg(2); WHEN others => port_io(2) <= 'Z'; END CASE;
-		CASE tris_reg(3) IS WHEN '0' => port_io(3) <= port_reg(3); WHEN others => port_io(3) <= 'Z'; END CASE;
-		CASE tris_reg(4) IS WHEN '0' => port_io(4) <= port_reg(4); WHEN others=> port_io(4) <= 'Z'; END CASE;
-		CASE tris_reg(5) IS WHEN '0' => port_io(5) <= port_reg(5); WHEN others=> port_io(5) <= 'Z'; END CASE;
-		CASE tris_reg(6) IS WHEN '0' => port_io(6) <= port_reg(6); WHEN others=> port_io(6) <= 'Z'; END CASE;
-		CASE tris_reg(7) IS WHEN '0' => port_io(7) <= port_reg(7); WHEN others=> port_io(7) <= 'Z'; END CASE;
-		
-		IF rd_en = '0' OR (abus_in /= port_addr OR abus_in /= alt_port_addr) THEN
-			latch <= port_io;
-		ELSE
-			latch <= "ZZZZZZZZ";
-		END IF;
-			
-		IF rd_en = '1' AND (abus_in = tris_addr OR abus_in = alt_tris_addr) THEN
-			dbus_out <= tris_reg;
-		ELSIF rd_en = '1' AND (abus_in = port_addr OR abus_in = alt_port_addr) THEN
-			dbus_out <= latch;
+		--Operacao de Leitura, dbus_out <= tris_reg ou latch
+		IF rd_en = '1' AND (abus_in = port_addr OR abus_in = alt_port_addr) THEN
+			dbus_out <= latch; --leitura de latch
+		ELSIF rd_en = '1' AND (abus_in = tris_addr OR abus_in = alt_tris_addr) THEN
+			dbus_out <= tris_reg; --leitura de tris_reg
 		ELSE
 			dbus_out <= "ZZZZZZZZ";
 		END IF;
 		
+		FOR i IN 0 TO 7 LOOP
+			IF tris_reg(i) = '0' THEN
+				port_io(i) <= port_reg(i);
+			ELSE
+				port_io(i) <= 'Z';
+			END IF;
+		END LOOP;
+			
 	END PROCESS;
-END ARCHITECTURE; 
+	
+	PROCESS(rd_en, port_io, abus_in)
+	BEGIN
+		IF NOT(rd_en = '1' AND (abus_in = port_addr OR abus_in = alt_port_addr)) THEN
+			latch <= port_io;
+		END IF;
+	END PROCESS;		
+		
+END arch;
